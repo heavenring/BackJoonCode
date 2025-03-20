cf9d7778591fd287906ba57fef3089c40602fa3c
heavenring <54017527+heavenring@users.noreply.github.com>
2025-03-20 16:15:30 +0900

    [level 1] Title: 폰켓몬, Time: 0.78 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,13 +1,26 @@
 def solution(nums):
-    answer = []
+    answer = 0
     
-    for poncketmon in nums:
-        # 보유한 폰켓몬 번호인지 확인
-        if poncketmon not in answer:
-            answer.append(poncketmon)
+    # 리스트의 중복 값 제거
+    pon_set = set(nums)
+    
+    # 보유 가능한 폰켓몬 종류가 보유 가능한 폰켓몬 수 보다 많은지 확인
+    if len(pon_set) >= len(nums) // 2: # 많다면 보유 가능 수 저장
+        answer = len(nums) // 2
+    else: # 적다면 폰켓몬 종류 저장
+        answer = len(pon_set)
+    
+    return answer
+    
+#     answer = []
+    
+#     for poncketmon in nums:
+#         # 보유한 폰켓몬 번호인지 확인
+#         if poncketmon not in answer:
+#             answer.append(poncketmon)
         
-        # 보유 가능한 폰켓몬 수의 도달했는지 확인
-        if len(answer) == len(nums) // 2:
-            break
+#         # 보유 가능한 폰켓몬 수의 도달했는지 확인
+#         if len(answer) == len(nums) // 2:
+#             break
     
-    return len(answer)
+#     return len(answer)
