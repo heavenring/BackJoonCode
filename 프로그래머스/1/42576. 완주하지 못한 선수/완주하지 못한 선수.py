04781efc08cf3668c2d66bf58f75dfcb7401775e
heavenring <54017527+heavenring@users.noreply.github.com>
2025-03-21 22:51:42 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.04 ms, Memory: 25.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,23 @@
+# list의 del 함수, index 함수 활용 -> 시간초과
+# for c in completion:
+#       del participant[participant.index(c)]
+
+
+def solution(participant, completion):
+    answer = ''
+    
+    # 두 리스트를 정렬
+    participant.sort()
+    completion.sort()
+    
+    # 정렬된 값이 서로 다르다면 해당 값 저장
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            answer = participant[i]
+            break
+    
+    # 비어있다면 마지막 값 저장
+    if not answer:
+        answer = max(participant)
+        
+    return answer
