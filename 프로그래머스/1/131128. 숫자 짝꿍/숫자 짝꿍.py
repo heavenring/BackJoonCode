b77bac55a64fab5b97fac47231210e321fcc089d
heavenring <54017527+heavenring@users.noreply.github.com>
2025-03-26 00:19:48 +0900

    [level 1] Title: 숫자 짝꿍, Time: 2356.04 ms, Memory: 26.5 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -4,18 +4,21 @@ def solution(X, Y):
     x_list = [0 for i in range(10)]
     y_list = [0 for i in range(10)]
     
+    # 어떤 숫자가 몇개 있는지 확인
+    # 내림차 순으로 숫자 개수 저장
     for num in X:
-        x_list[int(num)] += 1
+        x_list[9 - int(num)] += 1
         
     for num in Y:
-        y_list[int(num)] += 1
-    
-    x_list.reverse()
-    y_list.reverse()
+        y_list[9 - int(num)] += 1
+        
     
     for i in range(10):
+        # 더 적은 개수 == 겹치는 숫자 개수
         for num in range(min(x_list[i], y_list[i])):
             answer += str(9 - i)
+            
+            # answer == "0"일 경우 이후는 "0"만 붙기 때문에 break
             if answer == "0":
                 break
     
