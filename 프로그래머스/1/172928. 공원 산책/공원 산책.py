95e355ca940dae153c5d14eaedae90386b5d6c46
heavenring <54017527+heavenring@users.noreply.github.com>
2025-03-27 23:47:45 +0900

    [level 1] Title: 공원 산책, Time: 0.40 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -2,36 +2,67 @@
 # park를 순회하여 S의 위치 좌표를 answer에 저장한다
 # routes를 순회하며 w와 h를 계산한다
 # 계산 도중 -1 < answer[0] < w and -1 < answer[1] < h인지 확인한다
+# 이동하는 좌표 사이에 장애물이 있는지 확인한다
 # 못가는 좌표라면 이동한 좌표를 돌려놓는다
 
-def location_check_w(park, now_location, mv, h):
+
+# E 좌표로 이동할 경우
+def location_check_E(park, answer, mv):
     
-    if -1 < mv < len(park[0]):
+    # 이동 가능한 위치인가
+    if -1 < answer[1] + mv < len(park[0]):
         check = True
-        for i in range(now_location, mv + 1):
-            if park[h][i] == 'X':
-                return False    
-            
-        for i in range(mv, now_location + 1):
-            if park[h][i] == 'X':
+        # 이동하는데 장애물이 있는가
+        for j in range(answer[1], answer[1] + mv + 1):
+            if park[answer[0]][j] == 'X':
                 return False    
         
         return True
     else:
         return False 
-    
 
 
-def location_check_h(park, now_location, mv, w):
+# W 좌표로 이동할 경우
+def location_check_W(park, answer, mv):
     
-    if -1 < mv < len(park):
+    # 이동 가능한 위치인가
+    if -1 < answer[1] - mv < len(park[0]):
+        check = True 
+        # 이동하는데 장애물이 있는가
+        for j in range(answer[1] - mv, answer[1] + 1):
+            if park[answer[0]][j] == 'X':
+                return False    
+        
+        return True
+    else:
+        return False     
+
+    
+# S 좌표로 이동할 경우
+def location_check_S(park, answer, mv):
+    
+    # 이동 가능한 위치인가
+    if -1 < answer[0] + mv < len(park):
         check = True
-        for i in range(now_location, mv + 1):
-            if park[i][w] == 'X':
+        # 이동하는데 장애물이 있는가
+        for i in range(answer[0], answer[0] + mv + 1):
+            if park[i][answer[1]] == 'X':
                 return False
         
-        for i in range(mv, now_location + 1):
-            if park[i][w] == 'X':
+        return True    
+    else:
+        return False 
+    
+
+# N 좌표로 이동할 경우
+def location_check_N(park, answer, mv):
+    
+    # 이동 가능한 위치인가
+    if -1 < answer[0] - mv < len(park):
+        check = True
+        # 이동하는데 장애물이 있는가
+        for i in range(answer[0] - mv, answer[0] + 1):
+            if park[i][answer[1]] == 'X':
                 return False
         
         return True    
@@ -39,20 +70,14 @@ def location_check_h(park, now_location, mv, w):
         return False 
     
     
-
-    
 def solution(park, routes):
     answer = []
     
-    # park의 크기를 저장한다
-    w = len(park[0])
-    h = len(park)
-    
     # 로봇의 시작 위치를 찾는다
     i = 0
-    while not answer:
+    while not answer: # answer이 비어있지 않다면 반복
         j = 0
-        while j < w:
+        while j < len(park[0]):
             if park[i][j] == 'S':
                 answer = [i, j]
                 break
@@ -62,18 +87,22 @@ def solution(park, routes):
     
     # routes를 순회
     for data in routes:
-        # 방향과 이동칸 확인
+        # 방향에 따라 이동 가능한지 확인
         if data[0] == 'E':
-            if location_check_w(park, answer[1], answer[1] + int(data[2]), answer[0]):
+            # 이동 가능 여부 메소드에 park, 현재 좌표, 이동 거리를 인자로 전달 
+            if location_check_E(park, answer, int(data[2])):
                 answer[1] += int(data[2])
+                
         elif data[0] == 'W':
-            if location_check_w(park, answer[1], answer[1] - int(data[2]), answer[0]):
+            if location_check_W(park, answer, int(data[2])):
                 answer[1] -= int(data[2])
+                
         elif data[0] == 'S':
-            if location_check_h(park, answer[0], answer[0] + int(data[2]), answer[1]):
+            if location_check_S(park, answer, int(data[2])):
                 answer[0] += int(data[2])
+                
         elif data[0] == 'N':
-            if location_check_h(park, answer[0], answer[0] - int(data[2]), answer[1]):
+            if location_check_N(park, answer, int(data[2])):
                 answer[0] -= int(data[2])
         
         
