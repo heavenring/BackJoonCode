b737cdf5cae695d35ba7211341d8164c91d3af64
heavenring <54017527+heavenring@users.noreply.github.com>
2025-04-17 00:01:16 +0900

    [level 2] Title: 올바른 괄호, Time: 10.36 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,20 +1,48 @@
+# 2개의 풀이 있음
 def solution(s):
     answer = True
     
-    # dict를 사용해 계산
-    # 열고 제대로 닫힌다면 check는 0이 된다.
-    dic = {'(': 1, ')': -1}
+# 1.
+    if s[0] == ')':
+        return False
+    else:
+        check_list = [s[0]]
+        for char in s[1:]:
+            # check_list가 비어있을 때
+            if not len(check_list):
+                if char == ')': # 닫는 괄호가 나온다면 false
+                    return False
+                else: # 아니라면 리스트에 추가
+                    check_list.append(char)
+            # check_list에 문자가 있을 때
+            else:
+                if char == ')':
+                    del check_list[-1]
+                else:
+                    check_list.append(char)
+                    
+    if len(check_list):
+        return False
+    else:
+        return True
+                
     
-    check = 0
-    for char in s:
-        check += dic[char]
-        
-        # 만일 먼저 닫혀서 -1이 될 경우 뒷 문자는 확인하지 않는다.
-        if check == -1:
-            break;
     
-    # 문자를 확인한 결과 check가 0이 아니라면 잘못된 괄호 배열이다.
-    if check:
-        answer = False
+# 2. 
+#     # dict를 사용해 계산
+#     # 열고 제대로 닫힌다면 check는 0이 된다.
+#     dic = {'(': 1, ')': -1}
+    
+#     check = 0
+#     for char in s:
+#         check += dic[char]
         
-    return answer
+#         # 만일 먼저 닫혀서 -1이 될 경우 뒷 문자는 확인하지 않는다.
+#         if check == -1:
+#             break;
+    
+#     # 문자를 확인한 결과 check가 0이 아니라면 잘못된 괄호 배열이다.
+#     if check:
+#         answer = False
+        
+#     return answer
