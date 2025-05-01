e2cc323f4bfe5a4468814c9b9cfb589b8da85db3
heavenring <54017527+heavenring@users.noreply.github.com>
2025-05-01 16:48:20 +0900

    Update 신을 모시는 사당.py

--- 백준/Gold/27210. 신을 모시는 사당/신을 모시는 사당.py
+++ 백준/Gold/27210. 신을 모시는 사당/신을 모시는 사당.py
@@ -1,21 +1,20 @@
 num = int(input())
-rocks = input().split(' ')
+rocks = list(map(int, input().split(' ')))
 
 answer = 0
-rock_nums = ['1', '2']
-for rock_num in rock_nums:
+# 왼쪽 돌상, 오른쪽 돌상 구분
+for rock_num in range(1, 3):
     count = 0
-    for forward in rocks:
+    for forward in rocks: 
+        # 해당 돌상이 구하고자 하는 돌상과 같은 방향을 본다면
         if forward == rock_num:
             count += 1
         else:
             count -= 1
-
-            if count < 0:
-                count = 0
-
-        if answer < count:
-            answer = count
-
+            # 다른 방향을 보는 돌상이 더 많아 진다면 개수 초기화
+            count = max(count, 0)
+        
+        # 같은 방향을 보는 돌상이 가장 많을 때 저장
+        answer = max(answer, count)
 
 print(answer)
