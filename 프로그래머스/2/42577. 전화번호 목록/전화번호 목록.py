8accbb955ed5bfe26a31c44fc638aee3aaa3b0ed
heavenring <54017527+heavenring@users.noreply.github.com>
2025-05-04 14:07:46 +0900

    [level 2] Title: 전화번호 목록, Time: 111.28 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -11,14 +11,8 @@ def solution(phone_book):
     phone_book.sort()
     
     # 현재 번호와 다음 번호를 비교한다
-    for i in range(len(phone_book) - 1):
-        # 다음 번호의 시작 번호가 다르다면 continue
-        # 해당 로직으로 인해 접두어 번호가 접두사 번호보다 길어질 일이 없다
-        if phone_book[i][0] != phone_book[i + 1][0]:
-            continue
-        
-        # 접두어 번호를 포함하는지 확인
-        if phone_book[i] == phone_book[i + 1][:len(phone_book[i])]:
+    for now_num, next_num in zip(phone_book, phone_book[1:]):
+        if next_num.startswith(now_num):
             answer = False
             break
     
