5be6c17a9839fa2cc42956ef216811718c7c870a
heavenring <54017527+heavenring@users.noreply.github.com>
2025-05-06 14:07:26 +0900

    [Silver IV] Title: 아~파트 아파트, Time: 80 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/31797. 아～파트 아파트/아～파트 아파트.py
+++ 백준/Silver/31797. 아～파트 아파트/아～파트 아파트.py
@@ -7,19 +7,12 @@ if __name__ == "__main__":
     floor = list()
     for i in range(1, m + 1):
         left_hand, right_hand = map(int, input().split())
-        floor.append([left_hand, i])
-        floor.append([right_hand, i])
+        floor.append((left_hand, i))
+        floor.append((right_hand, i))
 
     # 사람들의 손 위치를 순서대로 정렬
     floor.sort()
-
-    # list를 deque로 변환
-    floor = deque(floor)
-
-    # n번째 사람 구하기
-    for i in range(n):
-        floor.append(floor.popleft())
-
-    answer = floor.pop()
-    print(answer[1])
-
+    
+    # 마지막으로 올라가는 손 위치 계산
+    index = n % len(floor) - 1
+    print(floor[index][1])
