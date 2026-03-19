a457aada9a2307c94943a35d3dcc07268d22140b
heavenring <54017527+heavenring@users.noreply.github.com>
2026-03-19 21:40:07 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    
+    
+    
+    return my_string.substr(0, n);
+}
