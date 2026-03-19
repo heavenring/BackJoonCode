e87536cfa2b8c135981ab3ee2f25070afe6024dd
heavenring <54017527+heavenring@users.noreply.github.com>
2026-03-19 17:58:57 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(string my_string, string target) {
+    int answer = 0;
+    
+    if (my_string.find(target) != -1){
+        answer = 1;
+    }
+    
+    return answer;
+}
