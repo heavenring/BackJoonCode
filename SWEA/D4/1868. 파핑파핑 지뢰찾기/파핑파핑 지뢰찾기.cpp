5053b75faaad91d0ae979fa0df370e372a70bbe2
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-05-07 09:59:29 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 334 ms, Memory: 8,740 KB -BaekjoonHub

---
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+int dx[] = { 1, 1, 0, -1, -1, -1, 0, 1 };
+int dy[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
+
+
+/* 주변 폭탄 개수 확인 및 폭탄이 없을경우 추가 탐색 */
+string zero_boom(vector<vector<string>>& board, int x, int y) {
+
+	//cout << "\n start location: " << x << " " << y << "\n";
+	int boom_cnt = 0;
+	for (int i = 0; i < 8; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+
+		if (nx < 0 || nx >= board.size() || ny < 0 || ny >= board.size()) {
+			continue;
+		}
+
+		//cout << "find nx location: " << nx << " " << ny << "\n";
+		//cout << "find nx value: " << board[nx][ny] << "\n";
+		if (board[nx][ny] == "*") {
+			//cout << "boom location: " << nx << " " << ny << "\n";
+			boom_cnt++;
+		} 
+	}
+
+	//cout << "boom_cnt: " << boom_cnt << "\n";
+
+	// 주변에 폭탄이 없을경우 주변 8칸도 탐색
+	if (!boom_cnt) {
+		board[x][y] = "0";
+
+		for (int i = 0; i < 8; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx < 0 || nx >= board.size() || ny < 0 || ny >= board.size()) {
+				continue;
+			}
+
+			if (board[nx][ny] == ".") {
+				board[nx][ny] = zero_boom(board, nx, ny);
+			}
+		}
+	}
+
+	return to_string(boom_cnt);
+}
+
+
+/* 나머지 폭탄 확인 */
+void boom_check(vector<vector<string>>& board, int x, int y) {
+	int boom_cnt = 0;
+	for (int i = 0; i < 8; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+
+		if (nx < 0 || nx >= board.size() || ny < 0 || ny >= board.size()) {
+			continue;
+		}
+
+		if (board[nx][ny] == "*") {
+			boom_cnt++;
+		}
+	}
+
+	board[x][y] = to_string(boom_cnt);
+}
+
+
+int main() {
+	int tc;
+	cin >> tc;
+
+	for (int t = 1; t <= tc; t++) {
+		int n;
+		cin >> n;
+
+		vector<vector<string>> board(n, vector<string>(n));
+		for (int i = 0; i < n; i++) {
+			string load_map;
+			cin >> load_map;
+
+			for (int j = 0; j < n; j++) {
+				board[i][j] = load_map[j];
+			}
+		}
+
+		//cout << "board\n";
+		//for (int i = 0; i < n; i++) {
+		//	for (int j = 0; j < n; j++) {
+		//		cout << board[i][j] << " ";
+		//	}
+		//	cout << "\n--------------------\n";
+		//}
+		//cout << "\n";
+
+		int answer = 0;
+		// 주변에 폭탄이 없는 칸 먼저 확인
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				//cout << "board location: " << i << " " << j << "\n";
+				//cout << "board value: " << board[i][j] << "\n";
+				if (board[i][j] == ".") {
+					zero_boom(board, i, j);
+
+					if (board[i][j] == "0") {
+						answer++;
+					}
+				}
+			}
+		}
+
+
+		//cout << "zero check board\n";
+		//for (int i = 0; i < n; i++) {
+		//	for (int j = 0; j < n; j++) {
+		//		cout << board[i][j] << " ";
+		//	}
+		//	cout << "\n--------------------\n";
+		//}
+		//cout << "\n";
+
+		// 나머지 칸 확인
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (board[i][j] == ".") {
+					boom_check(board, i, j);
+					answer++;
+				}
+			}
+		}
+
+
+		cout << "#" << t << " " << answer << endl;
+	}
+}
