5cb5c5962740a62a635b44c3880a3a654e51ec77
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-05-07 20:05:48 +0900

    [D3] Title: 원 안의 점, Time: 20 ms, Memory: 5,832 KB -BaekjoonHub

--- SWEA/D3/16910. 원 안의 점/원 안의 점.cpp
+++ SWEA/D3/16910. 원 안의 점/원 안의 점.cpp
@@ -13,16 +13,9 @@ int main() {
 		// (0, 0) 일때 1개 추가
 		long long answer = 1;
 
-		// x = 0일때 y * y <= n * n인 경우의 수
-		// y과 음수일 때와 양수 일 떄는 고려하여 2씩 더한다
-		for (int y = 1; y <= n; y++) {
-			answer += 2;
-		}
-
-		// y = 0일때 x * x <= n * n인 경우의 수
-		// x과 음수일 때와 양수 일 떄는 고려하여 2씩 더한다
-		for (int x = 1; x <= n; x++) {
-			answer += 2;
+		// x or y가 0일 때의 경우의 수
+		for (int i = 1; i <= n; i++) {
+			answer += 4;
 		}
 
 		// x > 0, y > 0 일 때 x * x + y * y <= n * n인 경우의 수
