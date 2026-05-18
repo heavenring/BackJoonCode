d4aabd501b752a20ff75188422b4822fa8c4b490
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-05-18 18:50:45 +0900

    [D3] Title: 이진수 표현, Time: 487 ms, Memory: 5,832 KB -BaekjoonHub

---
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+#define zisu 35
+
+using namespace std;
+
+int main() {
+	int tc;
+	cin >> tc;
+
+	for (int t = 1; t <= tc; t++) {
+		long long n, m;
+		cin >> n >> m;
+
+		string two_num = "";
+		int now_zisu = zisu;
+		while (now_zisu > -1) {
+			// 이진수 생성
+			if (m - (static_cast<long long>(1) << now_zisu) < 0) {
+				two_num += "0";
+			} else {
+				m -= (static_cast<long long>(1) << now_zisu);
+				two_num += "1";
+			}
+
+			now_zisu--;
+		}
+
+		bool check = true;
+		for (int i = two_num.size() - 1; i >= two_num.size() - n; i--) {
+			if (two_num[i] == '0') {
+				check = false;
+				break;
+			}
+		}
+
+		if (check) {
+			cout << "#" << t << " ON\n";
+		} else {
+			cout << "#" << t << " OFF\n";
+		}
+	}
+}
