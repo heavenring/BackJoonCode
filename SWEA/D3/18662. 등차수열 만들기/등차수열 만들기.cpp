f94842e17a43895e544abbd5b474b2d78020bc89
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-05-19 11:11:18 +0900

    [D3] Title: 등차수열 만들기, Time: 7 ms, Memory: 5,832 KB -BaekjoonHub

--- SWEA/D3/18662. 등차수열 만들기/등차수열 만들기.cpp
+++ SWEA/D3/18662. 등차수열 만들기/등차수열 만들기.cpp
@@ -21,16 +21,16 @@ int main() {
 		* b = (a + c) / 2
 		* c = 2 * b - a
 		* 이를 만족시키는 a, b, c를 각각 찾아 min을 사용한다
+		* 
+		* -- 추가 수정 사항 --
+		* 1. fixed << setprecision() 사용법 모름
+		* 2. 출력 예제가 0 이었으나, 출력 값은 0.0 이어야 함
 		*/
 
 		long double answer = LDBL_MAX;
-		long double x = 2 * b - c - a;
-		long double y = (a + c) / 2 - b;
-		long double z = 2 * b - a - c;
-
-		answer = min(abs(answer), abs(x));
-		answer = min(abs(answer), abs(y));
-		answer = min(abs(answer), abs(z));
+		answer = min(abs(answer), abs(2 * b - c - a));
+		answer = min(abs(answer), abs((a + c) / 2 - b));
+		answer = min(abs(answer), abs(2 * b - a - c));
 
 		cout << "#" << t << " ";
 		cout << fixed << setprecision(1) << answer;
